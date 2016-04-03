246a6fcb78da8253759daa368b644b86ef60ff4e
Caina Leao <cainaleao.uk@gmail.com>
2016-04-03 15:59:34 +0100

    Added more components

---
+++ resources/views/components/examples/profile/image.blade.php
@@ -0,0 +1,26 @@
+ <!-- Profile Image -->
+<div class="box box-primary">
+  <div class="box-body box-profile">
+    <img class="profile-user-img img-responsive img-circle" src="../../dist/img/user4-128x128.jpg" alt="User profile picture">
+
+    <h3 class="profile-username text-center">Nina Mcintire</h3>
+
+    <p class="text-muted text-center">Software Engineer</p>
+
+    <ul class="list-group list-group-unbordered">
+      <li class="list-group-item">
+        <b>Followers</b> <a class="pull-right">1,322</a>
+      </li>
+      <li class="list-group-item">
+        <b>Following</b> <a class="pull-right">543</a>
+      </li>
+      <li class="list-group-item">
+        <b>Friends</b> <a class="pull-right">13,287</a>
+      </li>
+    </ul>
+
+    <a href="#" class="btn btn-primary btn-block"><b>Follow</b></a>
+  </div>
+  <!-- /.box-body -->
+</div>
+<!-- /.box -->
