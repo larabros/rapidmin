6e172327450bc572156937de227c3aaaab9831a0
Caina Leao <cainaleao.uk@gmail.com>
2016-04-05 19:09:05 +0100

    fixes for bordered;

--- resources/views/components/tables/simple/bordered.blade.php
+++ resources/views/components/tables/simple/bordered.blade.php
@@ -1,4 +1,4 @@
- <div class="box">
+<div class="box">
   <div class="box-header with-border">
     <h3 class="box-title">Bordered Table</h3>
   </div>
--- resources/views/components/tables/simple/condensed-full-width.blade.php
+++ resources/views/components/tables/simple/condensed-full-width.blade.php
@@ -1,58 +1,47 @@
 <div class="box">
-  <div class="box-header">
-    <h3 class="box-title">Condensed Full Width Table</h3>
-  </div>
+    <div class="box-header {{ $bordered ? 'with-border' : ''   }}">
+        <h3 class="box-title"> {{ $title }} </h3>
+        @if ($pagination != null && $pagination['vertical'] == 'top' )
+             @include('rapidmin::components.ui.pagination', [ 'horizontal' => $pagination['horizontal'] ]);
+        @endif
+    </div>
   <!-- /.box-header -->
-  <div class="box-body no-padding">
-    <table class="table table-condensed">
-      <tr>
-        <th style="width: 10px">#</th>
-        <th>Task</th>
-        <th>Progress</th>
-        <th style="width: 40px">Label</th>
-      </tr>
-      <tr>
-        <td>1.</td>
-        <td>Update software</td>
-        <td>
-          <div class="progress progress-xs">
-            <div class="progress-bar progress-bar-danger" style="width: 55%"></div>
-          </div>
-        </td>
-        <td><span class="badge bg-red">55%</span></td>
-      </tr>
-      <tr>
-        <td>2.</td>
-        <td>Clean database</td>
-        <td>
-          <div class="progress progress-xs">
-            <div class="progress-bar progress-bar-yellow" style="width: 70%"></div>
-          </div>
-        </td>
-        <td><span class="badge bg-yellow">70%</span></td>
-      </tr>
-      <tr>
-        <td>3.</td>
-        <td>Cron job running</td>
-        <td>
-          <div class="progress progress-xs progress-striped active">
-            <div class="progress-bar progress-bar-primary" style="width: 30%"></div>
-          </div>
-        </td>
-        <td><span class="badge bg-light-blue">30%</span></td>
-      </tr>
-      <tr>
-        <td>4.</td>
-        <td>Fix and squish bugs</td>
-        <td>
-          <div class="progress progress-xs progress-striped active">
-            <div class="progress-bar progress-bar-success" style="width: 90%"></div>
-          </div>
-        </td>
-        <td><span class="badge bg-green">90%</span></td>
-      </tr>
-    </table>
-  </div>
+    <div class="box-body {{ $bordered ? '' : 'no-padding' }}"> <!-- !!!ONE DAY YOU MIGHT WANT TO USE ;) {{ $responsive ? 'table-responsive' : '' }} -->
+        <table class="table {{ $modifier or '' }} {{ $bordered ? 'table-bordered' : '' }}">
+            @if ($isDataTable)
+                <thead>
+            @endif
+            <tr>
+            @foreach ($table['columns'] as $column)
+                <th style='{{ $column['style'] or '' }}'>{!! $column['label'] !!}</th>
+            @endforeach
+            </tr>
+            @if ($isDataTable)
+                </thead>
+                <tbody>
+            @endif
+            @foreach ($table['data'] as $data)
+                <tr>
+                    <td>{!! $data['value'] !!}</td>
+                </tr>
+            @endforeach
+            @if ($isDataTable)
+                </tbody>
+                @if ($hasFooter)
+                <tfooter>
+                    <tr>
+                    @foreach ($table['columns'] as $column)
+                        <th style='{{ $column['style'] or '' }}'>{!! $column['label'] !!}</th>
+                    @endforeach
+                    </tr>
+                </tfooter>
+                @endif
+            @endif
+        </table>
+    </div>
   <!-- /.box-body -->
+    @if ($pagination != null && $pagination['vertical'] == 'bottom' )
+        @include('rapidmin::components.ui.pagination', [ 'horizontal' => $pagination['horizontal'] ]);
+    @endif
 </div>
 <!-- /.box -->
